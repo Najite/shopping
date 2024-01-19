2784153a0b8e3d5a8f46633951ab6835a21a6e30
Sawacha Emmanuel <sawachaemmanuel@gmail.com>
2024-01-19 12:42:56 +0100

    changes

--- src/components/banner-container.components.jsx
+++ src/components/banner-container.components.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+
+import { Container } from "@mui/material";
+
 import SliderComponent from "./carousel.components";
 
 
 const BannerContainer = () => {
     return (
+        <Container maxWidth="x1" style={{
+            height: '100vh', 
+            display: 'flex',
+            flexDirection: 'column'
+        }}>
         <SliderComponent />
+        </Container>
     )
 }
 
