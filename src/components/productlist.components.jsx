1ddc277196492b4228862eefe441cee0940af424
Sawacha Emmanuel <sawachaemmanuel@gmail.com>
2024-01-25 13:57:25 +0100

    first commit

--- src/components/productlist.components.jsx
+++ src/components/productlist.components.jsx
@@ -1,27 +1,84 @@
-import React from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import PropTypes from 'prop-types';
 
-import { Grid } from "@mui/material";
+import { CircularProgress, Container, Grid } from "@mui/material";
 
 
 import ProductItem from "./productitem.components";
 
 
-export const ProductList = ({products}) => (
+export const ProductList = ({products}) => {
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemPage = 4;
+    const pageSize = itemPage * currentPage;
+    const [loading, setLoading] = useState(false);
+
+    const observer = useRef();
+    const lastProductRef = useCallback(node => {
+        if (observer.current) observer.current.disconnet();
+        observer.current = new IntersectionObserver(entries => {
+            if (entries[0].isIntersecting) {
+                setCurrentPage(prevPage => prevPage + 1);
+            }
+        });
+        if (node) observer.current.observe(node);
+    }, []);
+
+    const visibleProducts = products.slice(0, pageSize);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const {scrollTop, clientHeight, scrollHeight} = document.documentElement;
+
+            if (scrollTop + clientHeight >= scrollHeight - 10 && !loading) {
+                setLoading(true);
+                setTimeout(() => {
+                    setCurrentPage(prevPage => prevPage + 1);
+                    setLoading(false);
+                }, 1000);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [loading])
+    return (
     <Grid container spacing={2} >
         {
-            products.map(product => (
-                <Grid item xs={12} sm={1} md={3} key={product.id} >
+            visibleProducts.map((product, index) => {
+                    return (
+                    <Grid item xs={12} sm={1} md={3} key={product.id} >
                     <ProductItem 
                     key={product.id}
                     product={product}
                     />
-                </Grid>
-
-            ))
+                    {loading && index === visibleProducts.length - 1 && (
+                        <Container
+                        style={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            marginTop: '10rem',
+                            
+                        }}
+                        >
+                            <CircularProgress />
+                        </Container>
+                    )}
+                    </Grid>
+                    );
+                }             
+            )
         }
+        <div ref={lastProductRef} style={{
+            height: '1px',
+            visibility: 'hidden'
+        }}></div>
     </Grid>
-);
+    )
+};
 
 ProductList.propTypes = {
     products: PropTypes.array.isRequired
--- src/components/productoverview.components.jsx
+++ src/components/productoverview.components.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { Container, CircularProgress } from "@mui/material";
+import { Container, CircularProgress, Typography } from "@mui/material";
 
 
 import useAPi from "../hooks/fetch-api";
@@ -10,18 +10,64 @@ const ProductOverview = () => {
     const url = 'https://nahjyte.pythonanywhere.com/api/product';
 
     const {data, isLoading, error} = useAPi(url);
+    const [sortedData, setSortedData] = useState(null);
+
     if (isLoading) {
         return (
-        <p>h</p>
-        ) 
+            <Container maxWidth='xl' style={{
+                marginTop: '10rem'
+                }}
+            >
+                <CircularProgress />
+            </Container>
+            ) 
     }
     if (error) {
-        return <p>Error: {error.message} </p>
+        <Container 
+            maxWidth="xl"
+            style={{
+                marginTop: '10rem'
+            }}
+        >
+            <Typography variant="h6" color="error">
+                Error: {error.message}
+            </Typography>
+        </Container>
+        }
+
+    const handleSort = (sortBy) => {
+        let sortedProducts;
+        console.log('fired')
+
+        switch (sortBy) {
+            case 'price':
+                sortedProducts = [...data].sort((a, b) => 
+                    a.price - b.price
+                );
+                break;
+            case 'name':
+                sortedProducts = [...data].sort((a, b) => 
+                a.name.localeCompare(b.name)
+                );
+                break;
+            default:
+                sortedProducts = [...data];
+        }
+
+        setSortedData(sortedProducts);
     }
     return (
     <Container maxWidth="xl" style={{marginTop: '10rem'}}>
-        { data && <ProductList products={data} />}
-        </Container>
+        { data && 
+            (
+                <>
+                <button onClick={() => handleSort('price')}> price</button>
+                <button></button>
+                <ProductList products={ sortedData || data} />
+                </>
+            )
+        }
+        </Container> 
     );
 };
 
